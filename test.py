44ce14d17527d65113bcd6cee0c1a38ec047de1b
Martin <martinborgesnogueira@gmail.com>
2020-11-19 09:40:54 -0300

    Creo un archivo para testear

---
+++ test.py
@@ -0,0 +1,19 @@
+from main import Main
+from account import Account
+from user import User
+
+#creando la administración
+admin = Main()
+
+
+#creando usuarios
+cuenta = Account()
+
+
+Martin = User('Martin','1234contraseña')
+Juan = User('Junito','123juanito')
+Pepe = User('Pepe12','contraseñaFacil')
+Carlo = User('Carlo_Kpo','55kapo')
+Jose = User('Jose_ok','jose44')
+Lautaro = User('Lauta','contraseña')
+
