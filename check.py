73eb60494889fdcfb88220e4517b0a1ee0d14f89
Martin <martinborgesnogueira@gmail.com>
2020-11-23 01:45:22 -0300

    AttributeError: Instancia de cheque

--- check.py
+++ check.py
@@ -4,13 +4,24 @@ from entity import Entity
 class Check:
     def __init(self, signature, memo, ammount, accountNumber, date, finalDate):
         self.drawer = []               
-        self.extensions = []         
+        self.extension = []         
         self.signature = signature
         self.memo = None
         self.ammount = ammount
         self.accountNumber = accountNumber
         self.date = date
         self.finalDate = None
+
+
+    #Agrega un librador a la lista
+    def registerDrawer(self, librador):
+        if isinstance(librador, Entity):
+            self.drawer.append(librador)
+
+
+    def registerExtension(self, recibidor):
+        if isinstance(recibidor, Entity):
+            self.extension.append(recibidor)
 
 
     def computes():
--- test.py
+++ test.py
@@ -2,13 +2,15 @@ from main import Main
 from account import Account
 from user import User
 from check import Check
+from person import Person
+from company import Company
 
 
 #creando la administración
 admin = Main()
 
 
-#creando usuarios
+#creando cuentas
 c1 = Account()
 c2 = Account()
 c3 = Account()
@@ -17,16 +19,43 @@ c5 = Account()
 c6 = Account()
 
 
-#cheque
-cheque = Check()
+#creando cheques
+ch1 = Check()
+ch2 = Check()
+ch2 = Check()
 
 
+    #Usuarios
 usr1 = User('Martin','1234contraseña')
 usr2 = User('Junito','123juanito')
 usr3 = User('Pepe12','contraseñaFacil')
 usr4 = User('Carlo_Kpo','55kapo')
 usr5 = User('Jose_ok','jose44')
 usr6 = User('Lauta','contraseña')
+
+
+    #Personas
+p1 = Person('Av. De las americas','1234')
+p1.name = 'Martin'
+p1.surname = 'Borges'
+
+p2 = Person('Av. Andresito 123', '12345')
+p2.name = 'Juanito'
+p2.surname = 'Mendez'
+
+
+    #Empresas
+em1 = Company('Juan Manuel de Rosas 123', '12')
+em1.businessName = 'Sport Gim'
+
+em2 = Company('Av. Misiones 981','32')
+em2.businessName = 'Bercomat'
+
+
+    #Cheque
+"p11 le envia dinero a p2"
+ch1.registerDrawer(p1)
+ch1.registerExtension(p2)
 
 
 #registrando usuarios
@@ -48,4 +77,9 @@ admin.accountRegister(c5)
 admin.accountRegister(c6)
 
 
-print(str(admin.accounts))
+#registrando cheques dentro de una cuenta
+c1.checkRegister(ch1)
+c1.checkRegister(ch2)
+
+"print(c1.checks)"
+"print(str(admin.accounts))"
